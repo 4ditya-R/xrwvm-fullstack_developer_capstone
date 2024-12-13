d54b66076fab7a0b0b49a2b23b8adefed45c7ff6
Aditya Raja <116998485+4ditya-R@users.noreply.github.com>
2024-12-13 20:15:46 +0530

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,10 +28,12 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://adityarajaof-8000.'\
-                 'theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://adityarajaof-8000.'\
-                        'theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', 'https://adityarajaof-8000.'
+                 'theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+                 'proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://adityarajaof-8000.'
+                        'theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+                        'proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -96,22 +98,22 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.'\
+        'django.contrib.auth.password_validation.'
         'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.'\
+        'django.contrib.auth.password_validation.'
         'MinimumLengthValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.'\
+        'django.contrib.auth.password_validation.'
         'CommonPasswordValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.'\
+        'django.contrib.auth.password_validation.'
         'NumericPasswordValidator',
     },
 ]
